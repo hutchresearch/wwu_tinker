eb95d66f2e30d8fea34d0171ffa0563da09c2384
shelbom <maxshelbourne@gmail.com>
2017-11-09 12:21:28 -0800

    new pip files

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 setup(name='tinker',
-      version='0.1',
+      version='0.1.0a1',
       description='Free hyperparameter optimization webservice',
       url='http://github.com/hutchresearch/tinker',
       author='WWU',
       author_email='brian.hutchinson@wwu.edu',
       license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3',
+      ],
       packages=['tinker'],
       install_requires=[
           'requests',
